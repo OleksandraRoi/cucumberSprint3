0498655cc2427a195ea4bf3ce60fdb0897915784
Katy <katia06@gmail.com>
2023-04-10 18:14:16 -0400

    econsent new commit-not finished

--- src/test/java/runners/SignUpCucumberRunner.java
+++ src/test/java/runners/SignUpCucumberRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 
-        tags = "@Scenario1 or @Scenario2 or @Scenario3 or @Scenario4",
+        tags = "@Scenario2 ",
         features = "src/test/resources",
         glue = "stepDefinitions",
         stepNotifications = true,
