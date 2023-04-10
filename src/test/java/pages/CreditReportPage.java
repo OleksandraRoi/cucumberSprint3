b25509bb1ae1173acd82ba45be9edf8092dc5d53
Nazar Grechukha <n.grechukha@gmail.com>
2023-04-10 18:58:53 -0400

    nazar3

--- src/test/java/pages/CreditReportPage.java
+++ src/test/java/pages/CreditReportPage.java
@@ -2,8 +2,12 @@ package pages;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utils.Driver;
 
 public class CreditReportPage {
+    public CreditReportPage(){
+        PageFactory.initElements(Driver.getDriver(), this);}
 
     public WebElement getCreditReportPage() {
         return CreditReportPage;
@@ -11,10 +15,16 @@ public class CreditReportPage {
 
     @FindBy (xpath = "(//label[@for='firstName12'])[3]")
     private WebElement CreditReportPage;
-
     public WebElement getYesCheckBox() {
         return YesCheckBox;
     }
+
+    public WebElement getCreditReportText() {
+        return CreditReportText;
+    }
+
+    @FindBy (xpath = "(//h6[@class='pb-50'])[2]")
+    private WebElement CreditReportText;
 
     @FindBy (xpath = "//label[@for='creditreport1']")
     private WebElement YesCheckBox;
